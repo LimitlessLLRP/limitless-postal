fc50bef37b33346ec360b73eb790d0ce190e0697
BigBearBeastTV <87631565+Bigbearbeasttv@users.noreply.github.com>
2021-11-14 15:29:50 -0700

    initial

---
+++ cl_commands.lua
@@ -0,0 +1,53 @@
+-- optimizations
+local ipairs = ipairs
+local upper = string.upper
+local format = string.format
+-- end optimizations
+
+---
+--- [[ Nearest Postal Commands ]] ---
+---
+
+TriggerEvent('chat:addSuggestion', '/postal', 'Set the GPS to a specific postal',
+             { { name = 'Postal Code', help = 'The postal code you would like to go to' } })
+
+RegisterCommand('postal', function(_, args)
+    if #args < 1 then
+        if pBlip then
+            RemoveBlip(pBlip.hndl)
+            pBlip = nil
+            QBCore.Functions.Notify("GPS System: You have removed the GPS location") 
+        end
+        return
+    end
+
+    local userPostal = upper(args[1])
+    local foundPostal
+
+    for _, p in ipairs(postals) do
+        if upper(p.code) == userPostal then
+            foundPostal = p
+            break
+        end
+    end
+
+    if foundPostal then
+        if pBlip then RemoveBlip(pBlip.hndl) end
+        local blip = AddBlipForCoord(foundPostal[1][1], foundPostal[1][2], 0.0)
+        pBlip = { hndl = blip, p = foundPostal }
+        SetBlipRoute(blip, true)
+        SetBlipSprite(blip, config.blip.sprite)
+        SetBlipColour(blip, config.blip.color)
+        SetBlipRouteColour(blip, config.blip.color)
+        BeginTextCommandSetBlipName('STRING')
+        AddTextComponentSubstringPlayerName(format(config.blip.blipText, pBlip.p.code))
+        EndTextCommandSetBlipName(blip)
+        
+        QBCore.Functions.Notify("GPS System: Set GPS Location")
+        
+    else
+        QBCore.Functions.Notify("GPS System: That Postal does not exist")
+        
+    end
+end)
+
